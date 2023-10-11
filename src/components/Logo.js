19e7e1e4907af225ac67ca4b3683ec0ae56ce72b
marcinkida97 <mkida97@gmail.com>
2023-10-11 19:23:26 +0200

    Frontend - Stworzenie Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Col, Container, Image, Row, Stack} from "react-bootstrap";
+
+function Logo() {
+    return (
+        <Stack direction="horizontal" gap={3}>
+            <Image className="p-2" src={require('./piggy-bank.png')} height={100} width={100}/>
+                <Col>
+                    <h1 className="justify-content-left defaultColor">Budget Buddy</h1>
+                    <h2 className="justify-content-left defaultColor">Your Financial Friend</h2>
+                </Col>
+        </Stack>
+    );
+}
+
+export default Logo;
